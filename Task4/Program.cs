d226586041c2e9188efe119ce4ff7646db834a6b
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-11-12 17:03:30 +0300

    solved Task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.WriteLine("Введите число а:");
+string userInputA = Console.ReadLine() ??"";
+int numberA = int.Parse(userInputA);
+
+Console.WriteLine("Введите число b: ");
+string userInputB = Console.ReadLine();
+int numberB = int.Parse(userInputB);
+
+Console.WriteLine("Введите число с: ");
+string userInputC = Console.ReadLine() ??"";
+int numberC = int.Parse(userInputC);
+
+int max = numberA;
+if (numberA > max) max = numberA;
+else if (numberB > max) max = numberB;
+else
+{
+    max= numberC;
+} 
+
+Console.Write("наибольшее из введенных чисел = ");
+Console.WriteLine(max);
+   

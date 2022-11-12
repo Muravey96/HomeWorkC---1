b6fe1181c4a82b3b6fc0597c4ed92a6d984a9228
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-11-12 18:21:55 +0300

    task 8 wasnt solved

---
+++ Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число:");
+string userInput= Console.ReadLine() ??"";
+int N = int.Parse(userInput);
+
+int count = 1;
+
+while (count < N);
+{
+    if (count%2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count = count ++;
+}
+
+Console.Write(N);
+Console.Write("->");
+Console.WriteLine(count);
+
+

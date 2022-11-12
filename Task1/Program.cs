4e0cb0090f519df85f7022540050434cd8d31570
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-11-12 16:32:11 +0300

    Solved Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число а:");
+string userInputA = Console.ReadLine() ??"";
+int numberA = int.Parse(userInputA);
+
+Console.WriteLine("Введите число b: ");
+string userInputB = Console.ReadLine();
+int numberB = int.Parse(userInputB); 
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+else max = numberB;
+
+Console.Write("max =");
+Console.WriteLine(max);
